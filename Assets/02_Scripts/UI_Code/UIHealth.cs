a197c524a1cc098c99ea529b6e7475cc70060cbf
Niclas Rummler <niclas.rummler@gmail.com>
2026-04-22 14:36:35 +0200

    Added Trailing to Health UI

--- Assets/02_Scripts/UI_Code/UIHealth.cs
+++ Assets/02_Scripts/UI_Code/UIHealth.cs
@@ -4,7 +4,12 @@ using UnityEngine.UI;
 public class UIHealth : MonoBehaviour
 {
     [SerializeField] private Health health;
-    [SerializeField] private Slider slider;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Slider trailSlider;
+    [SerializeField] private float trailDelay = 0.8f;
+    [SerializeField] private float trailSpeed = 0.4f;
+ 
+    private float trailTimer;
  
     private void OnEnable()
     {
@@ -12,8 +17,10 @@ public class UIHealth : MonoBehaviour
  
         health.OnHit += HandleHit;
         health.OnDeath += HandleDeath;
-        
         UpdateSlider();
+
+        if (trailSlider != null)
+            trailSlider.value = healthSlider.value;
     }
  
     private void OnDisable()
@@ -24,9 +31,30 @@ public class UIHealth : MonoBehaviour
         health.OnDeath -= HandleDeath;
     }
  
+    private void Update()
+    {
+        if (trailSlider == null) return;
+
+        if (trailTimer > 0f)
+        {
+            trailTimer -= Time.deltaTime;
+            return;
+        }
+
+        if (trailSlider.value > healthSlider.value)
+        {
+            trailSlider.value -= trailSpeed * Time.deltaTime;
+
+            if (trailSlider.value <= healthSlider.value)
+                trailSlider.value = healthSlider.value;
+        }
+    }
+ 
     private void HandleHit(HitData data)
     {
         UpdateSlider();
+
+        trailTimer = trailDelay;
     }
  
     private void HandleDeath()
@@ -36,10 +64,11 @@ public class UIHealth : MonoBehaviour
  
     private void UpdateSlider()
     {
-        if (slider != null && health != null)
+        if (healthSlider != null && health != null)
         {
-            slider.value = (float)health.Current / health.Max;
+            healthSlider.value = (float)health.Current / health.Max;
         }
     }
+    
     public void RefreshHealth() => UpdateSlider();
 }
