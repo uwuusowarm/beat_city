1727386aadcab7661c3221d518e3c4e3b04d3389
alihanGP <alihan.simsekoglu@games-academy.de>
2026-05-20 14:55:22 +0200

    Pickup HealPotion added

--- Assets/02_Scripts/GameSceneManager.cs
+++ Assets/02_Scripts/GameSceneManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GameSceneManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
         AudioManager.Instance.PlayMusic(MusicType.Stage1);
     }
