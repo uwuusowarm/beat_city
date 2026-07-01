1133395199dc05410437b61be91599fcca49921e
alihanGP <alihan.simsekoglu@games-academy.de>
2026-07-01 15:17:31 +0200

    animation hit timers fixed

---
+++ Assets/02_Scripts/Animation/AnimationEventRelay.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class AnimationEventRelay : MonoBehaviour
+{
+    [SerializeField] private PlayerCombat playerCombat;
+
+    private void Awake()
+    {
+        if (playerCombat == null)
+        {
+            playerCombat = GetComponentInParent<PlayerCombat>();
+
+            if (playerCombat != null)
+            {
+                Debug.LogWarning("ref to PlayerCombat is missing in AnimationEventRelay");
+            }
+        }
+
+        Debug.Log("[Relay] Awake. PlayerCombat found: " + (playerCombat != null));
+    }
+
+    public void EnableHitbox()
+    {
+        Debug.Log("[Relay] EnableHitbox called");
+
+        playerCombat.EnableHitbox();
+    }
+
+    public void DisableHitbox()
+    {
+        Debug.Log("[Relay] DisableHitbox called");
+
+        playerCombat.DisableHitbox();
+    }
+
+    public void FinishAttack()
+    {
+        Debug.Log("[Relay] FinishAttack called");
+
+        playerCombat.FinishAttack();
+    }
+}
